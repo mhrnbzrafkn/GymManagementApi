ae13ef03594d6287c56d76a655e9fc3079a1ac68
Mhrn <Mhrn@DESKTOP-GMJ72O2>
2021-11-29 22:45:21 -0800

    Coach Management Done.

---
+++ GymManagementApi/Repositories/Coaches/CoachEntityMap.cs
@@ -0,0 +1,19 @@
+﻿using KalCulate.Api.App.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KalCulate.Api.Repositories.Coaches
+{
+    public class CoachEntityMap : IEntityTypeConfiguration<Coach>
+    {
+        public void Configure(EntityTypeBuilder<Coach> builder)
+        {
+            builder.ToTable("Coaches");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.FirstName).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.LastName).HasMaxLength(75).IsRequired();
+        }
+    }
+}
---
+++ Migrations/Migrations/_202111300050_CreateCoachesTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Migrations.Migrations
+{
+    [Migration(202111300050)]
+    public class _202111300050_CreateCoachesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Coaches")
+                  .WithColumn("Id").AsInt32().PrimaryKey().NotNullable().Identity()
+                  .WithColumn("FirstName").AsString(50).NotNullable()
+                  .WithColumn("LastName").AsString(50).NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Coaches");
+        }
+    }
+}
